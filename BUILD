667b4d8641fe6d8b35d56e0f7a2ebb01820c18cb
Matt Matheson <mmatheson@uber.com>
2018-11-14 17:56:09 -0800

    add scoped_attribute

---
+++ BUILD
@@ -0,0 +1,11 @@
+#
+# Uber, Inc. (c) 2018
+#
+
+cc_library(
+    name = "spdlog",
+    hdrs = glob(["include/**/*.h"]),
+    includes = ["include"],
+    visibility = ["//visibility:public"],
+    defines = ["SPDLOG_SCOPED_ATTRIBUTES"],
+)
--- include/spdlog/details/attribute.h
+++ include/spdlog/details/attribute.h
@@ -14,6 +14,31 @@ using attr_key = std::string;
 attr_key key;
 attr_val value;
 };
+
+template <typename L>
+class scoped_attribute
+{
+public:
+    scoped_attribute(std::shared_ptr<L> logger, details::attribute&& attr) : logger_(std::move(logger))
+    {
+        logger_->push_attribute(std::move(attr));
+    }
+
+    ~scoped_attribute()
+    {
+        logger_->pop_attribute();
+    }
+
+private:
+    std::shared_ptr<L> logger_;
+};
 } // namespace details
 } // namespace spdlog
 #endif // SPDLOG_SCOPED_ATTRIBUTES
+
+#define CONCAT(x, y) x##y
+#define CONCAT2(x, y) CONCAT(x, y)
+#define ANONYMOUS_VAR(str) CONCAT2(str, __LINE__)
+#define SCOPED_ATTRIBUTE(...) \
+    spdlog::details::scoped_attribute ANONYMOUS_VAR(_scoped_attribute)(spdlog::default_logger(), __VA_ARGS__);
+
